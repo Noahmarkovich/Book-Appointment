f76fd24d59ed6b7aa3858d2a062b2f26107fe798
Noahmarkovich <noah.markovich.software@gmail.com>
2024-04-03 18:03:25 +0300

    rendering edit-content data

--- src/app/admin/manageContent/page.js
+++ src/app/admin/manageContent/page.js
@@ -1,7 +1,12 @@
 "use client";
-import { getData } from "@/app/services/admin.service";
+import {
+  AppointmentsDictionary,
+  HomePageDictionary,
+  OurTreatmentsDictionary,
+  getData,
+} from "@/app/services/admin.service";
 import { buttonTheme } from "@/styles/theme/muiTheme";
-import { Button, ThemeProvider } from "@mui/material";
+import { Button, TextField, ThemeProvider, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 
 export default function ManageContent() {
@@ -13,19 +18,31 @@ export default function ManageContent() {
     setData(currentData);
   }, []);
 
-  if (showedData) {
-    Object.entries(showedData?.content).map((key) => console.log(key));
+  function getSectionData(contentId) {
+    if (contentId === "home-page") {
+      setShowedData(HomePageDictionary);
+    }
+    if (contentId === "our-treatments") {
+      setShowedData(OurTreatmentsDictionary);
+    }
+    if (contentId === "appointments") {
+      setShowedData(AppointmentsDictionary);
+    }
   }
 
+  // if (showedData) {
+  //   Object.entries(showedData?.content).map((key) => console.log(key));
+  // }
+  console.log(showedData);
   return (
-    <section>
+    <section className="edit-content">
       <div className="data-controllers">
         <ThemeProvider theme={buttonTheme}>
           {data?.map((content) => {
             return (
               <Button
                 variant="text"
-                onClick={() => setShowedData(content)}
+                onClick={() => getSectionData(content.id)}
                 key={content.id}
                 color="black"
               >
@@ -37,12 +54,64 @@ export default function ManageContent() {
       </div>
       {showedData && (
         <div>
-          {/* <form>
-            {Object.entries(showedData?.content).map((key) => (
-              <div key={key[0]}>{key[1].title}</div>
-            ))}
-
-          </form> */}
+          <form className="edit-data-form">
+            {showedData.map((inputConfig) => {
+              return (
+                <div key={inputConfig.id} className="inputs">
+                  <Typography>{inputConfig.label}</Typography>
+                  {typeof inputConfig.value === "string" ||
+                  typeof inputConfig.value === "number" ? (
+                    <TextField
+                      value={inputConfig.value}
+                      name="inputConfig.id"
+                      multiline
+                      rows={4}
+                    />
+                  ) : (
+                    inputConfig.value.map((input) => {
+                      return (
+                        <div key={input.id} className="input">
+                          {input.title && (
+                            <TextField
+                              value={input.title}
+                              label="Title"
+                              multiline
+                              rows={4}
+                            />
+                          )}
+                          {input.p && (
+                            <TextField
+                              value={input.p}
+                              label="Paragraph"
+                              multiline
+                              rows={4}
+                            />
+                          )}
+                          {input.duration && (
+                            <TextField
+                              value={input.duration}
+                              label="Duration"
+                              type="number"
+                            />
+                          )}
+                          {input.price && (
+                            <TextField
+                              value={input.price}
+                              label="Price"
+                              type="number"
+                            />
+                          )}
+                          {/* {input.icon && (
+                            <input value={input.icon} type="image" />
+                          )} */}
+                        </div>
+                      );
+                    })
+                  )}
+                </div>
+              );
+            })}
+          </form>
         </div>
       )}
     </section>
--- src/app/services/admin.service.js
+++ src/app/services/admin.service.js
@@ -1,6 +1,145 @@
 setAdmins();
 setConstData();
 
+export const HomePageDictionary = [
+  {
+    id: "aboutUs.title",
+    value: "Welcome to our clinic - Laser Hair Removal Treatment Center!",
+    label: "First section main title",
+  },
+  {
+    id: "aboutUs.main",
+    value: [
+      {
+        id: "p1",
+        p: "Our clinic is a small and intimate establishment specializing in laser hair removal treatments. We believe in a personal and warm approach to every client, and we are committed to providing you with the best service in a pleasant and personal atmosphere.",
+      },
+      {
+        id: "p2",
+        p: "We invite you to join us and enjoy our professional and personal service, for a worry-free experience with impressive results. ",
+      },
+    ],
+    label: "Text about the clinic ",
+  },
+  {
+    id: "whyChooseUs.title",
+    value: "Why choose us ?",
+    label: "Second section main title",
+  },
+  {
+    id: "whyChooseUs.listContent",
+    value: [
+      {
+        id: "p3",
+        title: "Holistic approach by experts",
+        icon: "person-icon",
+        p: "Our clinic takes a holistic approach to laser hair removal, where our expert team considers individual needs and concerns, ensuring a comprehensive and personalized treatment experience.",
+      },
+      {
+        id: "p4",
+        title: "Customized treatment plan",
+        icon: "calendar-icon",
+        p: "Each client receives a customized treatment plan tailored to their unique hair type, skin tone, and desired outcome, guaranteeing optimal results and satisfaction.",
+      },
+      {
+        id: "p5",
+        title: "Keeping clean and sterilized",
+        icon: "clean-icon",
+        p: "We prioritize cleanliness and sterilization in every aspect of our clinic, maintaining rigorous standards to ensure a safe and hygienic environment for all our clients.",
+      },
+      {
+        id: "p6",
+        title: "Availability and easy access",
+        icon: "site-icon",
+        p: "With convenient online scheduling, we offer flexible appointment availability for our clients, ensuring easy access to our services at their preferred times.",
+      },
+    ],
+    label: "Reasons of why choose us",
+  },
+];
+
+export const OurTreatmentsDictionary = [
+  {
+    id: "treatments.title",
+    value: "Our treatments",
+    label: "Main title",
+  },
+  {
+    id: "treatments.p",
+    value:
+      "At our hair removal clinic, we prioritize gentle and effective care for our clients. Our state-of-the-art facility utilizes advanced laser technology, such as diode lasers or intense pulsed light (IPL), to target unwanted hair with precision and minimal discomfort. We understand the sensitivity of skin in these treatments, which is why our trained professionals ensure each session is tailored to individual needs, considering skin type and hair texture. Additionally, we offer a range of soothing products designed to enhance post-treatment comfort and promote skin recovery, ensuring a smooth and satisfying experience for every client.",
+    label: "Main paragraph",
+  },
+  {
+    id: "treatments.treatments",
+    value: [
+      {
+        id: "t1",
+        title: "בית שחי",
+        duration: 30,
+        price: 200,
+      },
+      {
+        id: "t2",
+        title: "רגליים",
+        duration: 60,
+        price: 400,
+      },
+      {
+        id: "t3",
+        title: "ידיים",
+        duration: 45,
+        price: 200,
+      },
+      {
+        id: "t4",
+        title: "שפם",
+        duration: 30,
+        price: 150,
+      },
+      {
+        id: "t5",
+        title: "מפשעות",
+        duration: 30,
+        price: 200,
+      },
+      {
+        id: "t6",
+        title: "בטן",
+        duration: 30,
+        price: 150,
+      },
+    ],
+    label: "Available Treatments",
+  },
+];
+
+export const AppointmentsDictionary = [
+  {
+    id: "durationBetweenTreatments",
+    value: 3,
+    label: "Duration between treatments",
+  },
+  {
+    id: "businessHours",
+    value: [
+      {
+        id: "business1",
+        daysOfWeek: [0, 1, 2, 4],
+        startTime: "09:00",
+        endTime: "18:00",
+      },
+      {
+        id: "business2",
+        daysOfWeek: [3],
+        startTime: "09:00",
+        endTime: "14:00",
+      },
+    ],
+    label: "Business Hours",
+  },
+];
+
 function setConstData() {
   let data = loadFromStorage("dataDB");
   if (!data) {
